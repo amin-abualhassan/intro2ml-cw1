b8d24c55b7e9d7168a7b63a5b1319839402b6429
Gifari Kautsar <kautsarg@ktsrg-mbp.local>
2025-10-31 10:13:36 +0000

    Add note for viz

--- dt_coursework/run.py
+++ dt_coursework/run.py
@@ -105,6 +105,7 @@ def run_one(data_path, name, k, seed, make_figures):
         print(f"\n---STEP 3: Generating full tree visualization for the CLEAN dataset ---")
         full_tree, depth, num_of_leaves = decision_tree_learning(X, y, depth=0)
         draw_tree(full_tree, filename=os.path.join(out_dir, 'tree_full_clean.png'))
+        print("Decision tree visualization for the clean data: Done")
     elif make_figures and name == 'noisy':
         print(f"\n---STEP 3: Generating full tree visualization for the NOISY dataset ---")
         print("Decision tree visualization is currently not supported for non clean data")
